82abe88f1e6ea67437523c69c3909daad80dc26f
Felix Atsma <felixatsma@gmail.com>
2017-04-26 02:23:57 +0200

    Add comments and store username and message in a seperate variable.

--- catz0rzbot.py
+++ catz0rzbot.py
@@ -11,11 +11,18 @@ port   = 6667
 chan   = "#sir_catz0rz"
 
 owner  = "sir_catz0rz"
-mods   = []
+mods   = ["catz0rzbot"]
 
 irc    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+com_dic = []
+
 def irc_connect(server, port):
+    """
+    Connects to the Twitch irc servers, to the channel specified in 'chan'
+    Prints the response to the attempted connection, this should just be the
+    Twitch welcome message.
+    """
     print("connecting to : " + server + ":" + str(port))
     print("channel       : " + chan)
     irc.connect((server, port))
@@ -28,42 +35,67 @@ def irc_connect(server, port):
     send("/me is now online")
 
 def send(msg):
+    """
+    Posts a message in the connected channel.
+    """
     irc.send(("PRIVMSG #sir_catz0rz :" + msg + "\n").encode())
     print("catz0rzbot\t: " + msg)
 
 def irc_read():
+    """
+    Read loop that reads messages posted in the channel and recovers the
+    username and message.
+    All commands are specified in here.
+
+    TODO: add !addcom command that adds simple text response commands stored
+          in a dictionary.
+
+    TODO: seperate words in message.
+    """
     regex = re.compile(r'^:([^\s]+)!.*#[^\s]+ :(.*)\r\n$')
     print("Chat:")
     while True:
         chat = irc.recv(2040)
         mesg = regex.findall(chat.decode())
         if len(mesg) != 0:
-            if (len(str(mesg[0][0])) > 7):
-                print(str(mesg[0][0]) + "\t: " + str(mesg[0][1]))
+            usr  = mesg[0][0]
+            txt  = mesg[0][1]
+            #if (mesg[0][2])
+            if (len(str(usr)) > 7):
+                print(str(usr) + "\t: " + str(txt))
             else:
-                print(str(mesg[0][0]) + "\t\t: " + str(mesg[0][1]))
-            if mesg[0][1] == "!ping":
+                print(str(usr) + "\t\t: " + str(txt))
+            if txt == "!ping":
                 send("pong")
-            elif mesg[0][1] == "!marco":
+            elif txt == "!marco":
                 send("polo")
-            elif mesg[0][1] == "!time":
+            elif txt == "!time":
                 send(time.asctime(time.localtime(time.time())))
-            elif mesg[0][1] == "!birthday":
-                send("Happy birthday " + mesg[0][0] + "!")
-            if mesg[0][1][0] == "!" and \
-               mesg[0][0] == owner or mesg[0][0] in mods:
-                if mesg[0][1] == "!stop":
+            elif txt == "!birthday":
+                send("Happy birthday " + usr + "!")
+            if txt[0] == "!" and \
+               usr == owner or usr in mods:
+                if txt == "!stop":
                     send("/me is now offline")
-                    print("User " + str(mesg[0][0]) + " called !stop")
+                    print("User " + str(usr) + " called !stop")
                     break
+                elif txt == "!addmod":
+                    pass
+                    #mods.append(mesg[arg])
 
 def irc_exit():
+    """
+    Closes the irc connection.
+    """
     print("exiting...")
     irc.send(("PART $sir_catz0rz\n").encode())
     irc.send(("QUIT\n").encode())
     irc.close()
 
 def main():
+    """
+    Initializes connection and read loop.
+    """
     irc_connect(server, port)
     irc_read()
     irc_exit()
