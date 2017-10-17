382dcbf6ce36910e0f5935a050a0bc3fd5d7c98b
Felix Atsma <felixatsma@gmail.com>
2017-10-17 20:58:21 +0200

    PEP8

--- catz0rzbot.py
+++ catz0rzbot.py
@@ -1,21 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
 import sys
 import socket
 import re
 import time
 
-user   = "catz0rzbot"
-passwd = (open("passwd", "r")).read()
+user = "catz0rzbot"
+passwd = open("passwd", "r").read()
 server = "irc.twitch.tv"
-port   = 6667
-chan   = "#sir_catz0rz"
+port = 6667
+chan = "#sir_catz0rz"
 
-owner  = "sir_catz0rz"
-mods   = ["catz0rzbot"]
+owner = "sir_catz0rz"
+mods = ["catz0rzbot"]
 
-irc    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-com_dic = {}
+command_dict = {}
+
 
 def irc_connect(server, port):
     """
@@ -34,12 +36,14 @@ def irc_connect(server, port):
     print(response.decode('unicode_escape'))
     send("/me is now online")
 
+
 def send(msg):
     """
     Posts a message in the connected channel.
     """
     irc.send(("PRIVMSG #sir_catz0rz :" + msg + "\n").encode())
     print("catz0rzbot\t: " + msg)
+
 
 def irc_read():
     """
@@ -53,8 +57,8 @@ def irc_read():
         chat = irc.recv(2040)
         mesg = regex.findall(chat.decode())
         if len(mesg) != 0:
-            usr  = mesg[0][0]
-            txt  = mesg[0][1].split()[0]
+            usr = mesg[0][0]
+            txt = mesg[0][1].split()[0]
             args = mesg[0][1].split()[1:]
 
             if (len(str(usr)) > 7):
@@ -62,8 +66,8 @@ def irc_read():
             else:
                 print(str(usr) + "\t\t: " + str(txt))
 
-            if txt in com_dic:
-                send(com_dic[txt])
+            if txt in command_dict:
+                send(command_dict[txt])
             elif txt == "!time":
                 send(time.asctime(time.localtime(time.time())))
             elif txt == "!birthday":
@@ -79,28 +83,31 @@ def irc_read():
                 elif txt == "!addmod":
                     mods.append(args[0])
                 elif txt == "!addcom":
-                    if args[0] in com_dic:
+                    if args[0] in command_dict:
                         send("Command already exists!")
                     else:
                         add_com(args[0], " ".join(args[1:]))
 
+
 def read_comms():
     """
-    Reads commands from 'comms' file into com_dic dictionary.
+    Reads commands from 'comms' file into command_dict dictionary.
     """
     comms = (open("comms", "r")).readlines()
 
     for line in comms:
-        com_dic.update({line.split()[0] : " ".join(line.split()[1:])})
+        command_dict.update({line.split()[0]: " ".join(line.split()[1:])})
+
 
 def add_com(com, text):
     """
-    Adds command to com_dic and 'comms' file.
+    Adds command to command_dict and 'comms' file.
     """
     comms = (open("comms", "a"))
-    com_dic.update({"!" + com : text})
+    command_dict.update({"!" + com: text})
     comms.write("!%s %s\n" % (com, text))
     send("Command added succesfully.")
+
 
 def irc_exit():
     """
@@ -111,6 +118,7 @@ def irc_exit():
     irc.send(("QUIT\n").encode())
     irc.close()
 
+
 def main():
     """
     Initializes connection and read loop.
@@ -119,6 +127,7 @@ def main():
     irc_connect(server, port)
     irc_read()
     irc_exit()
+
 
 if __name__ == "__main__":
     try:
